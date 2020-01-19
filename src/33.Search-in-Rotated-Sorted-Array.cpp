7c5447723c1ac8d37ac9b0a1c9c429ff0a640b59
ideawu <3202758+ideawu@users.noreply.github.com>
2020-01-19 15:04:26 +0800

    update

--- src/33.Search-in-Rotated-Sorted-Array.cpp
+++ src/33.Search-in-Rotated-Sorted-Array.cpp
@@ -1,6 +1,54 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 int search(vector<int>& nums, int target) {
-    
+	int s = 0;
+	int e = nums.size() - 1;
+	int t = target;
+	while(s <= e){
+		int mid = s + (e - s)/2;
+		if(nums[mid] == t){
+			return mid;
+		}
+		// either left sorted or right sorted
+		bool left_sorted = (nums[s] < nums[mid]);
+		if(left_sorted){
+			if(nums[s] <= t && t < nums[mid]){
+				e = mid - 1;
+			}else{
+				s = mid + 1;
+			}
+		}else{
+			if(nums[mid] < t && t <= nums[e]){
+				s = mid + 1;
+			}else{
+				e = mid - 1;
+			}
+		}
+	}
+	return -1;
 }
+
+int main(int argc, char **argv){
+	vector<int> nums;
+	int t;
+
+	nums = {4,5,6,7,0,1,2};
+	t = 0;
+	printf("%d\n\n", search(nums, 0));
+
+	nums = {4,5,6,7,0,1,2};
+	t = 0;
+	printf("%d\n\n", search(nums, 3));
+	
+	return 0;
+}
+
+// 主要考察各种条件组合
+
 /*
 Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
