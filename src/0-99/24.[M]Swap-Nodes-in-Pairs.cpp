4e31bffc7d80540d0b2259d76aea5b989bfb3eec
ideawu <3202758+ideawu@users.noreply.github.com>
2020-01-26 22:54:15 +0800

    update

--- src/0-99/24.[M]Swap-Nodes-in-Pairs.cpp
+++ src/0-99/24.[M]Swap-Nodes-in-Pairs.cpp
@@ -5,17 +5,39 @@
 
 using namespace std;
 
-int main(int argc, char **argv){
-	return 0;
-}
-
 /***********************************************************
 # 解题思路
 
+* 因为会改变头节点, 所以引入辅助头节点
+* flag 节点了插入点(插入其之后)
 ***********************************************************/
 ListNode* swapPairs(ListNode* head) {
+	ListNode *dummy = new ListNode(-1);
+	dummy->next = head;
 	
+	ListNode *flag = dummy;
+	ListNode *curr = flag->next;
+	while(curr && curr->next){
+		ListNode *next = curr->next;
+		curr->next = next->next;
+		next->next = flag->next;
+		flag->next = next;
+		
+		flag = curr;
+		curr = flag->next;
+	}
+	
+	return dummy->next;
 }
+
+int main(int argc, char **argv){
+	ListNode *head = build_list("1->2->3->4");
+	print_list(head);
+	head = swapPairs(head);
+	print_list(head);
+	return 0;
+}
+
 /*
 Given a linked list, swap every two adjacent nodes and return its head.
 
