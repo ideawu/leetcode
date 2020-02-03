4006817178405bcb6333ccba8037ecf3009dda96
ideawu <3202758+ideawu@users.noreply.github.com>
2020-02-03 21:13:01 +0800

    update

---
+++ src/100-199/TODO-109.[M]Convert-Sorted-List-to-Binary-Search-Tree.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <list>
+#include "../include/all.h"
+
+using namespace std;
+
+int main(int argc, char **argv){
+	return 0;
+}
+
+/***********************************************************
+# 解题思路
+
+***********************************************************/
+TreeNode* sortedListToBST(ListNode* head) {
+}
+
+/*
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted linked list: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
