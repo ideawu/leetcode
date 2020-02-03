4059cdbabfbd85537a965c77925b684be1cff69d
ideawu <3202758+ideawu@users.noreply.github.com>
2020-02-03 16:57:47 +0800

    update

---
+++ src/200-299/216.[M]Combination-Sum-III.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <list>
+#include "../include/all.h"
+
+using namespace std;
+
+/***********************************************************
+# 解题思路
+
+* 图遍历.
+* 不用预先生成数组, 只需要传递 [s,e] 即可.
+* 优化点: 1, 如果剩余的元素数量小于k则不递归;
+***********************************************************/
+void helper(vector<vector<int>> &ret, int s, int e, int k, int n, vector<int> &pre){
+	if(k == 0){
+		if(n == 0){
+			ret.push_back(pre);
+		}
+		return;
+	}
+	for(int i=s; i<=e-k+1; i++){
+		pre.push_back(i);
+		helper(ret, i+1, e, k-1, n-i, pre);
+		pre.pop_back();
+	}
+}
+vector<vector<int>> combinationSum3(int k, int n) {
+    vector<vector<int>> ret;
+	vector<int> pre;
+	helper(ret, 1, 9, k, n, pre);
+	return ret;
+}
+
+int main(int argc, char **argv){
+	print_matrix(combinationSum3(3, 7));
+	print_matrix(combinationSum3(3, 9));
+	return 0;
+}
+
+/*
+Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+
+Note:
+
+All numbers will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: k = 3, n = 7
+Output: [[1,2,4]]
+Example 2:
+
+Input: k = 3, n = 9
+Output: [[1,2,6], [1,3,5], [2,3,4]]
+*/
