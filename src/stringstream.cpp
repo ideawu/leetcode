9623eeb2a89a93ac6b2df19ef3c27aaa31507d95
ideawu <3202758+ideawu@users.noreply.github.com>
2020-02-04 23:19:26 +0800

    update

--- src/stringstream.cpp
+++ src/stringstream.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv){
 	stringstream ss("10\n1  2\n3 a4\n0 1");
 	string line;
 	//while(ss.getline(char *)){
-	while(std::getline(ss, line)){
+	while(std::getline(ss, line, '\n')){
 		printf("%s\n", line.c_str());
 		stringstream s2(line);
 		int a;
@@ -14,6 +14,16 @@ int main(int argc, char **argv){
 			printf(" > %d\n", a);
 		}
 	}
+	
+	char str[] = "a b - c   w";
+	const char *delimeter = " -";
+	char *tok = strtok(str, delimeter);
+	while(tok){
+		printf("[%s]\n", tok);
+		tok = strtok(NULL, delimeter);
+	}
+	printf("%s\n", str);
+	
 	return 0;
 }
 
